aad434d83e440b39f3b69bdd50b8ce3d2bc992cf
DeadZFrame <canaltay2001@windowslive.com>
2021-08-15 02:08:47 +0300

    Dialogue System

--- Assets/Scripts/UIManagement/TextWriter.cs
+++ Assets/Scripts/UIManagement/TextWriter.cs
@@ -5,36 +5,107 @@ using TMPro;
 
 public class TextWriter : MonoBehaviour
 {
-    private TextMeshProUGUI dialogText;
-    private string textToWrite;
-    private int characterIndex;
-    private float timePerCharacter;
-    private float timer;
+    [System.NonSerialized]
+    public List<TextWriterSingle> textWriterSingleList;
+    [System.NonSerialized] public static TextWriter instance;
+    [System.NonSerialized] public bool isWrited = false;
 
-    private void Update()
+
+    private void Awake()
     {
-        if(dialogText != null)
+        instance = this;
+        textWriterSingleList = new List<TextWriterSingle>();
+    }
+
+    public static void WriteText_Static(TextMeshProUGUI dialogText, string textToWrite, float timePerCharacter, bool invisibleCharacters, bool removeWriterBeforeAdd)
+    {
+        if (removeWriterBeforeAdd)
         {
-            timer -= Time.deltaTime;
-            if(timer <= 0f)
+            instance.RemoveWriter(dialogText);
+        }
+        instance.WriteText(dialogText, textToWrite, timePerCharacter, invisibleCharacters);
+    }
+
+    public void WriteText(TextMeshProUGUI dialogText, string textToWrite, float timePerCharacter, bool invisibleCharacters)
+    {
+        textWriterSingleList.Add(new TextWriterSingle(dialogText, textToWrite, timePerCharacter, invisibleCharacters));
+    }
+
+    public void RemoveWriter(TextMeshProUGUI dialogueText)
+    {
+        for (int i = 0; i < textWriterSingleList.Count; i++)
+        {
+            if (textWriterSingleList[i].GetProUGUI() == dialogueText)
             {
-                timer += timePerCharacter;
-                characterIndex++;
-                dialogText.text = textToWrite.Substring(0, characterIndex);
-                if(characterIndex >= textToWrite.Length)
-                {
-                    dialogText = null;
-                    return;
-                }
+                textWriterSingleList.RemoveAt(i);
+                i--;
             }
         }
     }
 
-    public void WriteText(TextMeshProUGUI dialogText, string textToWrite, float timePerCharacter)
+    private void Update()
     {
-        this.dialogText = dialogText;
-        this.textToWrite = textToWrite;
-        this.timePerCharacter = timePerCharacter;
-        characterIndex = 0;
+        for (int i = 0; i < textWriterSingleList.Count; i++)
+        {
+            bool destroyInstance = textWriterSingleList[i].Update();
+            if (destroyInstance)
+            {
+                textWriterSingleList.RemoveAt(i);
+                i--;
+                isWrited = true;
+            }
+            else
+            {
+                isWrited = false;
+            }
+        }
+
+    }
+
+    public class TextWriterSingle
+    {
+        private TextMeshProUGUI dialogText;
+        private string textToWrite;
+        private int characterIndex;
+        private float timePerCharacter;
+        private float timer;
+        private bool invisibleCharacters;
+
+        public TextWriterSingle(TextMeshProUGUI dialogText, string textToWrite, float timePerCharacter, bool invisibleCharacters)
+        {
+            this.dialogText = dialogText;
+            this.textToWrite = textToWrite;
+            this.timePerCharacter = timePerCharacter;
+            this.invisibleCharacters = invisibleCharacters;
+            characterIndex = 0;
+        }
+
+        public bool Update()
+        {
+            timer -= Time.deltaTime;
+            while (timer <= 0f)
+            {
+                timer += timePerCharacter;
+                characterIndex++;
+                string text = textToWrite.Substring(0, characterIndex);
+                if (invisibleCharacters)
+                {
+                    text += "<color=#00000000>" + textToWrite.Substring(characterIndex) + "</color>";
+                }
+                dialogText.SetText(text);
+
+                if (characterIndex >= textToWrite.Length)
+                {
+
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public TextMeshProUGUI GetProUGUI()
+        {
+            return dialogText;
+        }
     }
 }
--- Assets/Scripts/UIManagement/UIManager.cs
+++ Assets/Scripts/UIManagement/UIManager.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] private TextWriter textWriter; 
     public TextMeshProUGUI dialogText;
+    public Image exorcist, demon;
+
+    private bool exorcistTalking;
+
+    public string[] dialogueArray;
+    string dialogue;
+
+    private int dialogueIndex = 0;
 
     private void Awake()
     {
@@ -15,6 +23,43 @@ public class UIManager : MonoBehaviour
 
     private void Start()
     {
-        textWriter.WriteText(dialogText, "My goals are beyond of your understanding!", .3f);
+        dialogue = dialogueArray[dialogueIndex];
+        TextWriter.WriteText_Static(dialogText, dialogue, .1f, true, true);
+    }
+
+    private void Update()
+    {
+        DialogueManager();
+    }
+
+    public void DialogueManager()
+    {
+        if (dialogueIndex != dialogueArray.Length - 1)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (TextWriter.instance.isWrited)
+                {
+                    dialogueIndex++;
+                    dialogue = dialogueArray[dialogueIndex];
+                    TextWriter.WriteText_Static(dialogText, dialogue, .1f, true, true);
+                }
+                else
+                {
+                    TextWriter.WriteText_Static(dialogText, dialogue, 0f, true, true);
+                }
+            }
+        }
+
+        if (exorcistTalking)
+        {
+            exorcist.gameObject.SetActive(true);
+            demon.gameObject.SetActive(false);
+        }
+        else
+        {
+            exorcist.gameObject.SetActive(false);
+            demon.gameObject.SetActive(true);
+        }
     }
 }
