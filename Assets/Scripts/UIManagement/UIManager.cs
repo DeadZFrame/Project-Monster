dc5730aa046d2f70f574a79466ffbf80c6579021
DeadZFrame <canaltay2001@windowslive.com>
2021-08-16 14:18:49 +0300

    Update UIManager.cs

--- Assets/Scripts/UIManagement/UIManager.cs
+++ Assets/Scripts/UIManagement/UIManager.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    Scene scene;
+
     public TextMeshProUGUI dialogText, fadingText;
     public Image exorcist, demon;
     public GameObject pausePanel, dialoguePanel;
@@ -13,7 +16,7 @@ public class UIManager : MonoBehaviour
     [System.NonSerialized] public bool exorcistTalking, onDialogue, trigger, first;
 
     public string[] dialogueArray;
-    string dialogue;
+    [System.NonSerialized] public string dialogue;
 
     private int dialogueIndex = 0;
 
@@ -25,12 +28,21 @@ public class UIManager : MonoBehaviour
 
     private void Start()
     {
+        scene = SceneManager.GetActiveScene();
         dialogue = dialogueArray[dialogueIndex];
         TextWriter.WriteText_Static(dialogText, dialogue, .1f, true, true);
         dialogueIndex++;
 
         trigger = false;
-        onDialogue = true;
+        if (scene.name.Equals("MainScene"))
+        {
+            onDialogue = true;
+        }
+        else if (scene.name.Equals("LEVEL 2"))
+        {
+            onDialogue = false;
+        }
+
         first = true;
     }
 
@@ -50,47 +62,94 @@ public class UIManager : MonoBehaviour
         else
             dialoguePanel.SetActive(false);
 
-        if (dialogueIndex != dialogueArray.Length - 1 && onDialogue)
+        if (scene.name.Equals("MainScene"))
         {
-            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0) || trigger)
+            if (dialogueIndex != dialogueArray.Length - 1 && onDialogue)
             {
-                if (TextWriter.instance.isWrited && dialogueIndex != 4)
+                if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0) || trigger)
                 {
-                    dialogue = dialogueArray[dialogueIndex];
-                    TextWriter.WriteText_Static(dialogText, dialogue, .04f, true, true);
-                    dialogueIndex++;
-                    first = true;
+                    if (TextWriter.instance.isWrited && dialogueIndex != 4 && dialogueIndex != 17)
+                    {
+                        dialogue = dialogueArray[dialogueIndex];
+                        TextWriter.WriteText_Static(dialogText, dialogue, .04f, true, true);
+                        dialogueIndex++;
+                        first = true;
+                    }
+                    else if (trigger)
+                    {
+                        dialogue = dialogueArray[dialogueIndex];
+                        TextWriter.WriteText_Static(dialogText, dialogue, .04f, true, true);
+                        trigger = false;
+                        dialogueIndex++;
+                    }
+                    else
+                    {
+                        TextWriter.WriteText_Static(dialogText, dialogue, 0f, true, true);
+                    }
                 }
-                else if (trigger)
-                {
-                    dialogue = dialogueArray[dialogueIndex];
-                    TextWriter.WriteText_Static(dialogText, dialogue, .04f, true, true);
-                    trigger = false;
-                    dialogueIndex++;
-                }
-                else
-                {
-                    TextWriter.WriteText_Static(dialogText, dialogue, 0f, true, true);
-                }
+                fadingText.gameObject.SetActive(true);
             }
-            fadingText.gameObject.SetActive(true);
-        }
-        else
-        {
-            fadingText.gameObject.SetActive(false);
-            onDialogue = false;
+            else
+            {
+                fadingText.gameObject.SetActive(false);
+                onDialogue = false;
+            }
+
+            if (dialogueIndex == 4 && first)
+            {
+                onDialogue = false;
+                first = false;
+            }
+            if (dialogueIndex == 17 && first)
+            {
+                onDialogue = false;
+                first = false;
+            }
+
+            if (dialogueIndex <= 5 || dialogueIndex == 14 || dialogueIndex == 18)
+                exorcistTalking = true;
+            else
+                exorcistTalking = false;
         }
 
-        if (dialogueIndex == 4 && first)
+        if (scene.name.Equals("LEVEL 2"))
         {
-            onDialogue = false;
-            first = false;
-        }
+            if (dialogueIndex != dialogueArray.Length - 1 && onDialogue)
+            {
+                if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0) || trigger)
+                {
+                    if (TextWriter.instance.isWrited)
+                    {
+                        dialogue = dialogueArray[dialogueIndex];
+                        TextWriter.WriteText_Static(dialogText, dialogue, .04f, true, true);
+                        dialogueIndex++;
+                        first = true;
+                    }
+                    else if (trigger)
+                    {
+                        dialogue = dialogueArray[dialogueIndex];
+                        TextWriter.WriteText_Static(dialogText, dialogue, .04f, true, true);
+                        trigger = false;
+                        dialogueIndex++;
+                    }
+                    else
+                    {
+                        TextWriter.WriteText_Static(dialogText, dialogue, 0f, true, true);
+                    }
+                }
+                fadingText.gameObject.SetActive(true);
+            }
+            else
+            {
+                fadingText.gameObject.SetActive(false);
+                onDialogue = false;
+            }
 
-        if (dialogueIndex <= 5 || dialogueIndex == 14)
-            exorcistTalking = true;
-        else
-            exorcistTalking = false;
+            if (dialogueIndex == 1 || dialogueIndex == 4 || dialogueIndex == 5)
+                exorcistTalking = true;
+            else
+                exorcistTalking = false;
+        }
 
         if (!exorcistTalking)
         {
